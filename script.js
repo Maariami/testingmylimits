ee607e6b12407ff850abddafa6356375c3026d5a
Maariami <mariam8bidzinashvili@gmail.com>
2026-03-21 23:26:30 +0400

    mairmai

---
+++ script.js
@@ -0,0 +1,20 @@
+// const boxcontainer = document.querySelector(".boxes");
+
+// fetch("https://fakestoreapi.com/products")
+//   .then((response) => response.json())
+//   .then((products) => {
+//     loadApi(products);
+//   })
+//   .catch((error) => console.error("ther eis and error", error));
+
+// function loadApi(products) {
+//   boxcontainer.innerHTML = "";
+
+//   products.forEach((product) => {
+//     boxcontainer.innerHTML += `  <div class="box">
+//         <img src="${product.image}" alt="${product.title}" />
+//         <p class="title">${product.title}</p>
+//         <p class="title">$${product.price}</p>
+//       </div>`;
+//   });
+// }
